be8773418c6c3f092fac9faff0e957c72d9e4deb
BetaCatPro <zj20162325@163.com>
2023-06-12 16:31:20 +0800

    feat: user profile 相关 api

---
+++ userop-web/global/global.go
@@ -0,0 +1,22 @@
+package global
+
+import (
+	"go-shop-api/userop-web/config"
+	"go-shop-api/userop-web/proto"
+
+	ut "github.com/go-playground/universal-translator"
+)
+
+var (
+	Trans ut.Translator
+
+	ServerConfig *config.ServerConfig = &config.ServerConfig{}
+
+	NacosConfig *config.NacosConfig = &config.NacosConfig{}
+
+	GoodsSrvClient proto.GoodsClient
+
+	MessageClient proto.MessageClient
+	AddressClient proto.AddressClient
+	UserFavClient proto.UserFavClient
+)
